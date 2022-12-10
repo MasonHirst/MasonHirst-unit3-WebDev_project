e44bc5c69ddc72ef83e406a88e87d591aa83b40a
Mason Hirst <mason.j.hirst@gmail.com>
2022-12-10 12:51:14 -0700

    the project is almost done. The form submits, but the radio button select doesn't quite work

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react";
+import { Formik } from 'formik'
 
 
 const NewRecipeScreen = () => {
@@ -10,83 +11,159 @@ const NewRecipeScreen = () => {
       cookTime: "",
       serves: "",
       ingredients: [],
-      instructions: "",
+      instructions: ""
    }
 
    const onSubmit = (values) => {
+      values.ingredients = ingredients
       console.log(values)
    }
 
-   const [ingredientData, setIngredientsData] = useState([])
-   const ingredientRef = useRef()
+   const nameRef = useRef()
    const quantityRef = useRef()
 
-   const ingredientAddHandler = () => {
-      let quantity = quantityRef.current.value
-      let ingredient = ingredientRef.current.value
-      if (quantity !== '' && ingredient !== '') {
-         setIngredientsData([...ingredientData, [quantity + ' ' + ingredient]])
-         quantityRef.current.value = ''
-         ingredientRef.current.value = ''
-         ingredientRef.current.focus()
-      } else {
-         alert('You need to fill out Ingredient and Quantity fields')
-      }
+
+   const [ingredients, setIngredients] = useState([])
+   const [name, setName] = useState('')
+   const [quantity, setQuantity] = useState('')
+
+   const addIngredient = () => {
+      setIngredients([...ingredients, {name, quantity}])
+      nameRef.current.value = ''
+      quantityRef.current.value = ''
+      nameRef.current.focus()
    }
 
-   // console.log(ingredientData)
-   const addedIngredientsDisplay = ingredientData.map((item) => {
-         return <li>{item}</li>
-      })
+   const addedIngredientsDisplay = ingredients.map((item) => {
+      return <li>{item.quantity} {item.name}</li>
+   })
    
   return (
     <section className="main-display-container">
       <h1 className="add-recipe__title">Tell us about your Recipe!</h1>
-      
-      <form className="add-recipe__form">
-         <div className="add-recipe__first-inputs">
-            <input className="full-input input-text" placeholder="Name"/>
-            <input className="full-input input-text" placeholder="ImageURL"/>
-         </div>
 
-         <div className="radio-btns-div">
-            <span className="radio-spans">
-               <input className="radio-btn input-text" type='radio' id="cook-option" name="recipe-type" value="Cook"/>
-               <label className="input-text" for="cook-option">Cook</label>
-            </span>
-            <span className="radio-spans">
-               <input className="radio-btn input-text" type='radio' id="bake-option" name="recipe-type" value="Bake"/>
-               <label className="input-text" for="bake-option">Bake</label>
-            </span>
-            <span className="radio-spans">
-               <input className="radio-btn input-text" type='radio' id="drink-option" name="recipe-type" value="Drink"/>
-               <label className="input-text" for="drink-option">Drink</label>
-            </span>
-         </div>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+         {({values, handleChange, handleSubmit}) => (
+               <form className="add-recipe__form" onSubmit={handleSubmit}>
+                  <div className="add-recipe__first-inputs">
+                     <input 
+                        className="full-input input-text" 
+                        placeholder="Name" 
+                        onChange={handleChange} 
+                        value={values.recipeName}
+                        name="recipeName"
+                     />
+                     <input 
+                        className="full-input input-text" 
+                        placeholder="ImageURL" 
+                        onChange={handleChange} 
+                        value={values.imageURL}
+                        name="imageURL"
+                     />
+                  </div>
 
-         <div className="form__basic-numbers-div">
-            <input className="smaller-input input-text" placeholder="Prep Time"/>
-            <input className="smaller-input input-text" placeholder="Cook Time"/>
-            <input className="smaller-input input-text" placeholder="Serves"/>
-         </div>
-         
-         <div className="add-recipe__ingredient-form">
-            <section className="add-recipe__inputs-ul">
-               <div className="add-recipe__list-inputs">
-                  <input className="full-input input-text" placeholder="Ingredient" ref={ingredientRef}/>
-                  <input className="full-input input-text" placeholder="Quantity" ref={quantityRef}/>
-               </div>
-               <ul className="add-recipe__ul">
-                  {addedIngredientsDisplay}
-               </ul>
-            </section>
-            <button type="button" className="blue-btn orange-color" onClick={ingredientAddHandler}>Add Another</button>
-         </div>
+                  <div className="radio-btns-div">
+                     <span className="radio-spans">
+                        <input 
+                           className="radio-btn input-text" 
+                           type='radio' 
+                           id="cook-option" 
+                           name="type" 
+                           value={values.type} 
+                           onChange={handleChange}
+                        />
+                        <label className="input-text" for="cook-option">Cook</label>
+                     </span>
+                     <span className="radio-spans">
+                        <input 
+                           className="radio-btn input-text" 
+                           type='radio' 
+                           id="bake-option" 
+                           name="type" 
+                           value={values.type} 
+                           onChange={handleChange}
+                        />
+                        <label className="input-text" for="bake-option">Bake</label>
+                     </span>
+                     <span className="radio-spans">
+                        <input 
+                           className="radio-btn input-text" 
+                           type='radio' 
+                           id="drink-option" 
+                           name="type" 
+                           value={values.type} 
+                           onChange={handleChange}
+                        />
+                        <label className="input-text" for="drink-option">Drink</label>
+                     </span>
+                  </div>
 
-         <textarea className="input-text" rows="5" name="instructions-input" placeholder="What are the instructions?"/>
-         
-         <button className="blue-btn">Save</button>
-      </form>
+                  <div className="form__basic-numbers-div">
+                     <input 
+                        className="smaller-input input-text" 
+                        placeholder="Prep Time" 
+                        onChange={handleChange} 
+                        value={values.prepTime}
+                        name="prepTime"
+                     />
+                     <input 
+                        className="smaller-input input-text" 
+                        placeholder="Cook Time" 
+                        onChange={handleChange} 
+                        value={values.cookTime}
+                        name="cookTime"
+                     />
+                     <input 
+                        className="smaller-input input-text" 
+                        placeholder="Serves" 
+                        onChange={handleChange} 
+                        value={values.serves}
+                        name="serves"
+                     />
+                  </div>
+                  
+                  <div className="add-recipe__ingredient-form">
+                     <section className="add-recipe__inputs-ul">
+                        <div className="add-recipe__list-inputs">
+                           <input 
+                              className="full-input input-text" 
+                              placeholder="Ingredient"
+                              onChange={(e) => setName(e.target.value)}
+                              ref={nameRef}
+                           />
+                           <input 
+                              className="full-input input-text" 
+                              placeholder="Quantity"
+                              onChange={(e) => setQuantity(e.target.value)}
+                              onkeydown={(event) => {
+                                 if (event.keyCode === 13) {
+                                    addIngredient()
+                                 }
+                              }}
+                              ref={quantityRef}
+                           />
+                        </div>
+                        <ul className="add-recipe__ul">
+                           {addedIngredientsDisplay}
+                        </ul>
+                     </section>
+                     <button type="button" className="blue-btn orange-color" onClick={addIngredient}>Add Another</button>
+                  </div>
+
+                  <textarea 
+                     className="input-text" 
+                     rows="5" 
+                     placeholder="What are the instructions?" 
+                     onChange={handleChange} 
+                     value={values.instructions}
+                     name="instructions"
+                  />
+                  
+                  <button className="blue-btn">Save</button>
+               </form>
+            )
+         }
+      </Formik>
     </section>
   )
 }
